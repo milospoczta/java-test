d0b2fccfe75cc833304d0959ee8779e44fac4f51
Grzegorz Milos <milospoczta@gmail.com>
2012-11-17 23:22:51 +0100

    playing with shortcuts

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,19 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("test");
+		System.out.println("test");
+		
+//		i++ is the same as i = i + 1
+//		i++ is the same as i += 1
+		int j = 0;
+		j += 2; // j = j + 2;
+		j++;
+		
+		j--;
+		
+		j *= 3;
+		
+		System.out.println(j);
 	}
 
 }
